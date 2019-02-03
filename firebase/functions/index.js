922dee738db5ee23119708d81cb6cf36fa48222a
Oktay Sen <senokt.16@gmail.com>
2019-02-03 18:25:39 +0000

    I55 (#58)
    * Removed firebase folder
    
    * Removed firebase functions deployment from travis.yaml
    
    * Random IntelliJ changes

--- firebase/functions/index.js
+++
@@ -1,9 +0,0 @@
-const functions = require('firebase-functions');
-
-exports.onCreate = functions.firestore
-    .document('orders/{orderID}')
-    .onCreate((snap, context) => {
-      return snap.ref.set({
-        name: "BOB"
-      }, {merge: true});
-    });
