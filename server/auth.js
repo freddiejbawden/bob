3d6de9d29393568c3ac89bca366f596ccf0ffc4b
Oktay Sen <senokt.16@gmail.com>
2019-02-20 03:13:53 +0000

    Moved auth documentation to README.md

--- server/auth.js
+++ server/auth.js
@@ -1,37 +1,5 @@
 const model = require('./model')
 
-/*
-
-// Usage:
-
-const auth = require('./auth')
-
-// Without auth:
-app.get((req, res) => {
-    res.send('Hello world!')
-})
-
-// With auth (for any logged in user):
-app.get(auth.any((req, res) => {
-    const currentUser = req.user
-    res.send('Hello ' + currentUser.username)
-}))
-
-// With auth (merchants only):
-app.get(auth.merchant((req, res) => {
-    const currentUser = req.user
-    res.send('Hello ' + currentUser.username)
-}))
-// Note: You can also use: auth.customer and auth.robot in this way
-
-// Select your own user groups:
-app.get(auth(['merchant', 'robot'], (req, res) => {
-    const currentUser = req.user
-    res.send('Hello ' + currentUser.username)
-}))
-
-*/
-
 const authFactory = model => (userTypes, routeHandler) => (req, res, next) => {
     const username = req.header('username')
     if (!username) {
