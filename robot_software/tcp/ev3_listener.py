05ea6702b3e0f1be3c1b69c31ca14a99b344ace4
freddiejbawden <freddiejbawden@gmail.com>
2019-03-07 17:47:04 +0000

    Robot moves on TCP

--- robot_software/tcp/ev3_listener.py
+++ robot_software/tcp/ev3_listener.py
@@ -1,5 +1,5 @@
 import socket
-
+from followLine import FollowLine
  # Get local machine name
 PORT = 65432 # Port to listen on (non-privileged ports are > 1023)
 
@@ -7,14 +7,15 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     HOST  = socket.gethostbyname(socket.gethostname())
     s.bind((HOST, PORT))
     print("Listening on {}:{}".format(HOST,PORT))
-    s.listen()
+    s.listen(2)
     conn, addr = s.accept()
     with conn:
         print('Connected by', addr)
         while True:
             data = conn.recv(1024)
-            if not data:
-                print('break')
-                break
-            print(data)
-            conn.sendall(data)
+            if data:
+                print(data)
+                conn.sendall(data)
+                robot = FollowLine()
+                robot.run_forwards(2, False)
+                
