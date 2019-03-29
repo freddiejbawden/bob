36702c51193ac1cb86f5b22d0ecba5bc9dc06e3a
Frederick Bawden <frederickbawden@c113211.wlan.net.ed.ac.uk>
2019-03-29 15:11:39 +0000

    Updated Pathfinding

--- server/robot-pathfinding.js
+++ server/robot-pathfinding.js
@@ -45,14 +45,14 @@ generate_grab_instruction = (height) => {
             command: 'grab',
             parameters: {'height':height}
         }
-    
+    x
 }
 
 generate_lift_instruction = (start, end) => {
     return {
         command: 'lift',
         parameters: {
-            height: Math.abs(start - end)
+            height: end - start
         }
     }
 }
@@ -85,41 +85,87 @@ generate_movement_instruction = (start, end) => {
         }
     }
 }
-convert_order_to_job = (order, robot, warehouse_grid) => {
+package_items_into_groups = (item_list) => {
+    var sorted_items = {"tiny":[],"small":[],"large":[]}
+    for(var i = 0; i < item_list.length; i++) {
+        var item = item_list[i]
+        for (var j = 0; j < item['quantity']; j++) {
+            sorted_items[item['size']].push(item)
+        }
+    }
+    var item_groups = []
+    item_groups.push(sorted_items.large)
+    for (var i = 0; i < sorted_items.small.length; i+=2) {
+        if (i + 1 < sorted_items.small.length) {
+            console.log("here")
+            item_groups.push([sorted_items.small[i],sorted_items.small[i+1]])
+        } else {
+           if (sorted_items.tiny.length >= 1) {
+               item_groups.push([sorted_items.tiny.pop(),sorted_items.small[i]])
+           } else {
+               
+               item_groups.push([sorted_items.small[i]])
+           }
+        }
+    }
+    for (var i = 0; i < sorted_items.tiny.length; i+=3) {
+        to_push = [sorted_items.tiny[i]]
+        if (i + 1 < sorted_items.tiny.length) {
+            to_push.push(sorted_items.tiny[i+1])
+        }
+        if (i + 2 < sorted_items.tiny.length) {
+            to_push.push(sorted_items.tiny[i+2])
+        }
+        item_groups.push(to_push)
+    }
+    console.log(item_groups)
+    return item_groups
+}
+ convert_order_to_job = (order, robot, warehouse_grid) => {
     if (order == undefined) {
         return {}
     }
     var robot_pos = robot['location']
-    var item_list = order['items']
-
+    
+    const item_list = order['items']
+    const item_groups = package_items_into_groups(item_list)
     var job = {
         id: order['_id'],
         instruction_set: []
     }
     var robot_xy = [robot_pos['x'], robot_pos['y'], robot_pos['z']]
-    for (var i = 0; i < item_list.length; i++) {
-        var current_item = item_list[i]['position']
-        var item_xy = [current_item['x'], current_item['y'], current_item['z']]
-
-        var path = pathfind_to_point(robot_xy, item_xy, warehouse_grid)
-        if (path == [] || path == undefined) {
-            return {}
+    const robot_home_xy = [robot['home_x'],robot['home_y']]
+    // collect item groups
+    for (var i = 0; i < item_groups.length; i++) {
+        var current_group = item_groups[i]
+        var path = []
+        for (var j = 0; j < current_group.length; j++) {
+            var current_item = current_group[j]['position']
+            var item_xy = [current_item['x'], current_item['y'], current_item['z']]
+            
+            path = pathfind_to_point(robot_xy, item_xy, warehouse_grid)
+            if (path == [] || path == undefined) {
+                return {}
+            }
+            //go to position and grab
+            for (var p = 1; p < path.length; p++) {
+                job['instruction_set'].push(generate_movement_instruction(path[p - 1], path[p]))
+            }
+          
+              
+            job['instruction_set'].push(generate_grab_instruction(item_xy[2]))
+           
+            robot_xy = [item_xy[0],item_xy[1],0]
         }
-        //go to position and grab
+        // return home
+        path = pathfind_to_point(robot_xy, robot_home_xy, warehouse_grid)
         for (var p = 1; p < path.length; p++) {
             job['instruction_set'].push(generate_movement_instruction(path[p - 1], path[p]))
         }
-        if (robot_xy[2] != item_xy[2]) {
-            job['instruction_set'].push(generate_lift_instruction(robot_xy[2], item_xy[2]))
-        }
-        job['instruction_set'].push(generate_grab_instruction(item_xy[2]))
-        // return home
-        for (var p = path.length - 1; p > 0; p--) {
-            job['instruction_set'].push(generate_movement_instruction(path[p], path[p - 1]))
-        }
         job['instruction_set'].push(generate_drop_instruction())
+        robot_xy = [robot_home_xy[0],robot_home_xy[1],0]
     }
-
+    console.log(job)
     return job
 }
 module.exports.get_robot_path = (order, robot, warehouse) => {
